b4b0b21dee6f3640da8b82dafbb4b5ff130f2b96
Samuel Banya <sbanya@fastmail.com>
2022-12-02 14:53:28 -0600

    Adding latest changes

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,19 @@
 require_relative './stack'
 
-# your code here
+# Write a balancing_parentheses method in the lib/balancing_parentheses.rb file that:
+# Takes as input a string consisting of some number of ( and ) characters
+def balancing_parentheses(str)
+    # Creates an instance of the Stack class
+    stack = Stack.new
+
+    str.chars.each do |c|
+        # Using the stack, checks the string to determine whether the parentheses inside it are balanced
+        if stack.peek == '(' && c == ')'
+            stack.pop
+        # Returns the number of additional parentheses that would need to be added to the string to make it balanced
+        else
+            stack.push(c)
+        end
+    end
+    stack.size
+end
